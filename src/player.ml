bfd596e707b2054e22380a8c728b28076e360c4f
Fionna <fyc3@cornell.edu>
2021-10-12 21:54:53 -0400

    added some to player and table

--- src/player.ml
+++ src/player.ml
@@ -1,11 +1,18 @@
+type play =
+  | Raise
+  | Calls
+  | Folds
+
 type t = {
   id : int;
   name : string;
   cards : Deck.card list;
+  chips : int;
+  position : play option;
 }
 
-let new_player (name : string) (id : int) = { id; name; cards = [] }
+let new_player (id : int) (name : string) =
+  { id; name; cards = []; chips = 0; position = None }
 
 let add_cards (player : t) (lst : Deck.card list) =
   { player with cards = player.cards @ lst }
-
--- src/table.ml
+++ src/table.ml
@@ -9,7 +9,16 @@ type t = {
   pot : int;
   deck : Deck.deck;
   stage : stage;
+  small_blind : int;
+  big_blind : int;
 }
 
 let init (plst : Player.t list) : t =
-  { players = plst; pot = 0; deck = Deck.new_deck; stage = Preflop }
+  {
+    players = plst;
+    pot = 0;
+    deck = Deck.new_deck;
+    stage = Preflop;
+    small_blind = 1;
+    big_blind = 2;
+  }
