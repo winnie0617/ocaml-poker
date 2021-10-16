d490dabc75034f88e3ce08d8fd645b16ee8687c3
Yilun Zhou <@cornell.edu>
2021-10-16 14:47:56 -0400

    deck.mli changes

--- src/deck.mli
+++ src/deck.mli
@@ -2,6 +2,20 @@ type card
 
 type deck = card list
 
+val empty: unit -> deck
+
+(** [push card deck] takes in a new card and places in the front of the existed deck*)
+val push: card -> deck -> deck
+
+(** [peek deck] returns the first card of the deck but does not change the deck*)
+val peek: deck -> card
+
+(** [pop deck] removes the first card from the deck and returns the rest of the deck*)
+val pop: deck -> card
+
+(** [len deck] returns the length of the deck*)
+val len: deck -> int
+
 val new_deck : deck
 
 (** draw is a tuple of (drawn card * updated deck) *)
