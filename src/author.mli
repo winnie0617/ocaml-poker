ba0399a0980aa4515f1bcd68df18a5f41236c8c1
Fionna <fyc3@cornell.edu>
2021-10-19 20:35:25 -0400

    add author files and install instructions

---
+++ src/author.mli
@@ -0,0 +1,23 @@
+(** CS 3110 Fall 2021 OCaml Texas Hold'Em Poker
+
+    @author Fionna Chen (fyc3), Winnie Chow (wc459), Yilun Zhou (yz464)*)
+
+(************************************************************
+
+   Academic Integrity Statement
+
+   Us, the team named in the author comment above, have fully reviewed the
+   course academic integrity policies.  We have adhered to those policies in
+   solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If I did engage in such collaboration, here is the
+   list of other students with whom I collaborated, and a brief summary of that
+   collaboration:
+
+   - none
+
+ ************************************************************)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours you worked on this assignment. *)
