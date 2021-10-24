0241357e9c69c60876fa6e3384ce26c2d138ccb3
Yilun Zhou <@cornell.edu>
2021-10-24 14:58:17 -0400

    Added compare module

---
+++ src/compare.ml
@@ -0,0 +1,13 @@
+type ratings =
+|Royal_Straight_Flush
+|Straight_Flush
+|Four_Of_A_Kind
+|Full_House
+|Flush
+|Straight
+|Three_Of_A_Kind
+|Two_Pair
+|One_Pair
+|High_Card
+
+let compare (players:Player.t list) (com_cards:Deck.card list)=
